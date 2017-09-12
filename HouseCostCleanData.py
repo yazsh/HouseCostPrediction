d893f6d57997f0cb34d083647b473ccd444cbd8b
Yazen2017 <yazen.shunnar@gmail.com>
2017-09-12 18:57:03 -0400

    Train and test data prepared for model

--- HouseCostCleanData.py
+++ HouseCostCleanData.py
@@ -21,4 +21,20 @@ train_features_object = pd.get_dummies(train_features_object)
 
 train_features = pd.concat([train_features_object, train_features_numeric],1)
 
-train_features
+test_features = pd.read_csv("/Users/yazen/Desktop/datasets/HouseCostData/test.csv")
+test_features.drop("Id", axis=1, inplace=True)
+
+test_features_numeric = test_features.select_dtypes(exclude=['object'])
+columns = test_features_numeric.columns
+test_features_numeric = test_features_numeric.fillna(0)
+min_max_scaler = preprocessing.MinMaxScaler()
+test_features_numeric = min_max_scaler.fit_transform(test_features_numeric)
+test_features_numeric = pd.DataFrame(test_features_numeric,columns = columns)
+
+#one hot objects
+test_features_object = test_features.select_dtypes(include=['object'])
+test_features_object = pd.get_dummies(test_features_object)
+
+test_features = pd.concat([test_features_object, test_features_numeric],1)
+
+test_features.head
